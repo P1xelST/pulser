b284bd72d56516434f4f6f8d5f4f91d7d1d5fce2
Sergey Trifonov <se.trif0nov@yandex.com>
2022-11-28 13:51:25 +0300

    compleate

--- src/js/script.js
+++ src/js/script.js
@@ -68,4 +68,64 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+    
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                }
+            },
+            messages: {
+                name: "Быстро вводи имя!",
+                phone: "Цифры мне в базу!",
+                email: {
+                    required: "О да, халявная почта :D",
+                    email: "Вы где-то ошиблись!",
+                }
+            }
+        });
+    }
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize(),
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1200) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $('a[href^="#"]').click(function() {
+        const _href = $(this).attr("href");
+        $('html, body').animate({scrollTop: $(_href).offset().top+'px'});
+        return false;
+    })
 });
